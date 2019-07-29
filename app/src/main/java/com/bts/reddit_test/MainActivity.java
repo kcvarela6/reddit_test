022f6eeccfaae616976fabc4ea92f92f6fdca064
Karina Campero <karinac@bluetrailsoft.com>
2019-07-29 13:46:03 -0300

    primeras clases

--- app/src/main/java/com/bts/reddit_test/MainActivity.java
+++ app/src/main/java/com/bts/reddit_test/MainActivity.java
@@ -1,13 +1,13 @@
 package com.bts.reddit_test;
 
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+
+
+
 }
